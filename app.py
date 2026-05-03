67c37b09c6681c2e3f0ce5309a5385dbde22ce38
zonemaxtv840-source <zonemaxtv840@gmail.com>
2026-05-03 00:44:27 -0400

    Update app.py

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ def generar_excel_web(items, cliente, ruc):
     return output.getvalue()
 
 # --- 3. INTERFAZ PRINCIPAL ---
-st.title("💎 Buscador Diamante QTC")
+st.title("💎Consola de Operaciones QTC Pro - Business Suite")
 st.sidebar.header("📂 Carga de Archivos")
 
 f_p = st.sidebar.file_uploader("1. Catálogo de Precios (Excel)", type=['xlsx'])
