0d303d09d5bde793255081829e29fa4fda2d5a34
zonemaxtv840-source <zonemaxtv840@gmail.com>
2026-05-03 00:49:50 -0400

    Update app.py

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ def generar_excel_web(items, cliente, ruc):
     return output.getvalue()
 
 # --- 3. INTERFAZ PRINCIPAL ---
-st.title("💎QTC Smart Sales - Business Suite")
+st.title("📊 QTC Smart Sales - Consola de Operaciones")
 st.sidebar.header("📂 Carga de Archivos")
 
 f_p = st.sidebar.file_uploader("1. Catálogo de Precios (Excel)", type=['xlsx'])
