74fc44a6af6fdef6d230684a155f3a1f9593894f
zonemaxtv840-source <zonemaxtv840@gmail.com>
2026-05-14 15:14:30 -0400

    Update test_conexion.py

--- test_conexion.py
+++ test_conexion.py
@@ -3,7 +3,7 @@ from supabase import create_client
 
 # TUS DATOS (cámbialos por los tuyos)
 SUPABASE_URL = "https://aolttzscbzckfkceqjey.supabase.co"
-SUPABASE_KEY = "aquí_va_tu_clave_anon_public"  # <-- PEGA TU CLAVE AQUÍ
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFvbHR0enNjYnpja2ZrY2VxamV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg3ODMzOTIsImV4cCI6MjA5NDM1OTM5Mn0.5cDNWqZ1YRBs_MHNZntKjNJ-kApiyOtwNb0xaifire0"  # <-- PEGA TU CLAVE AQUÍ
 
 # Intentar conectar
 try:
